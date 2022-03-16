ddbf8ca68cd2fc49caf013f7da2e1ebe92b5f30b
navI-kryabik <sergei30062003@gmail.com>
2022-03-16 03:12:02 +0300

    nComps

--- main.c
+++ main.c
@@ -294,8 +294,159 @@ void timeExperiment() {
     }
 }
 
+long long getSelectionSortNComp(int *a, size_t n) {
+    long long nComps = 0;
+    for (int i = 0; ++nComps && i < n; i++) {
+        int min = a[i];
+        int minIndex = i;
+        for (int j = i + 1; ++nComps && j < n; j++)
+            if (++nComps && a[j] < min) {
+                min = a[j];
+                minIndex = j;
+            }
+        if (++nComps && i != minIndex)
+            swap(&a[i], &a[minIndex]);
+    }
+    return nComps;
+}
+
+long long getBubbleSortNComp(int arr[], int n) {
+    long long nComps = 0;
+    for (size_t i = 0; ++nComps && i < n - 1; i++)
+        for (size_t j = 0; ++nComps && j < n - i - 1; j++)
+            if (++nComps && arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+    return nComps;
+}
+
+long long getInsertionSortNComp(int *a, const size_t size) {
+    long long nComps = 0;
+    for (size_t i = 1; ++nComps && i < size; i++) {
+        int t = a[i];
+        int j = i;
+        while (++nComps && j > 0 && a[j - 1] > t) {
+            a[j] = a[j - 1];
+            j--;
+        }
+        a[j] = t;
+    }
+    return nComps;
+}
+
+long long getShellSortNComp(int *array, int size) {
+    long long nComps = 0;
+    for (int s = size / 2; ++nComps && s > 0; s /= 2) {
+        for (int i = s; ++nComps && i < size; ++i) {
+            for (int j = i - s; ++nComps && j >= 0 && array[j] > array[j + s]; j -= s) {
+                int temp = array[j];
+                array[j] = array[j + s];
+                array[j + s] = temp;
+            }
+        }
+    }
+    return nComps;
+}
+
+long long getCombSortNComp(int *a, const size_t n) {
+    long long nComps = 0;
+    size_t step = n;
+    int swapped = 1;
+    while (++nComps && step > 1 || swapped) {
+        if (++nComps && step > 1)
+            step /= 1.24733;
+        swapped = 0;
+        for (size_t i = 0, j = i + step; ++nComps && j < n; i++, j++)
+            if (++nComps && a[i] > a[j]) {
+                swap(&a[i], &a[j]);
+                swapped = 1;
+            }
+    }
+    return nComps;
+}
+
+long long getRadixSortNComp_(int *l, int *r, int N) {
+    long long nComps = 0;
+    int k = (32 + N - 1) / N;
+    int M = 1 << N;
+    int sz = r - l;
+    int *b = (int *) malloc(sizeof(int) * sz);
+    int *c = (int *) malloc(sizeof(int) * M);
+    for (int i = 0; ++nComps && i < k; i++) {
+        for (int j = 0; ++nComps && j < M; j++)
+            c[j] = 0;
+
+        for (int *j = l; ++nComps && j < r; j++)
+            c[digit(*j, i, N, M)]++;
+
+        for (int j = 1; ++nComps && j < M; j++)
+            c[j] += c[j - 1];
+
+        for (int *j = r - 1; ++nComps && j >= l; j--)
+            b[--c[digit(*j, i, N, M)]] = *j;
+
+        int cur = 0;
+        for (int *j = l; ++nComps && j < r; j++)
+            *j = b[cur++];
+    }
+    free(b);
+    free(c);
+
+    return nComps;
+}
+
+long long getRadixSortNComp(int *a, const size_t n) {
+    return getRadixSortNComp_(a, a + n, 8);
+}
+
+void NCompsExperiment() {
+// описание функций сортировки
+    SortFuncNComps sorts[] = {
+            {getBubbleSortNComp,    " getBubbleSortNComp "},
+            {getSelectionSortNComp, " getSelectionSortNComp "},
+            {getInsertionSortNComp, " getInsertionSortNComp "},
+            {getShellSortNComp,     " getShellSortNComp "},
+            {getCombSortNComp,      " getCombSortNComp "},
+            {getRadixSortNComp,     " getRadixSortNComp "}
+    };
+
+    const unsigned FUNCS_N = ARRAY_SIZE(sorts);
+
+// описание функций генерации
+    GeneratingFunc generatingFuncs[] = {
+// генерируется случайный массив
+            {generateRandomArray,      " random "},
+// генерируется массив 0, 1, 2, ..., n - 1
+            {generateOrderedArray,     " ordered "},
+// генерируется массив n - 1, n - 2, ..., 0
+            {generateOrderedBackwards, " orderedBackwards "}
+    };
+    const unsigned CASES_N = ARRAY_SIZE
+                             (generatingFuncs);
+
+// запись статистики в файл
+    for (size_t size = 10000; size <= 100000; size += 10000) {
+        printf(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+        printf(" Size : %d\n", size);
+        for (int i = 0; i < FUNCS_N; i++) {
+            for (int j = 0; j < CASES_N; j++) {
+// генерация имени файла
+                static char filename[128];
+                sprintf(filename,
+                        "%s%s_nComps ",
+                        sorts[i].name, generatingFuncs[j].name);
+                checkNComps(sorts[i]
+                                    .sort,
+                            generatingFuncs[j].generate,
+                            size, filename);
+            }
+        }
+        printf("\n");
+    }
+}
+
+
 int main() {
-    timeExperiment();
+    NCompsExperiment();
 }
 
 
